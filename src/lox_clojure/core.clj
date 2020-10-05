a1349e83e2bf5fd850d3f830a2585948aaeca5fb
Sumit Ahluwalia <sumit@thisisalice.com>
2020-10-05 15:41:04 +0530

    Chap 6 complete, without error handling

--- src/lox_clojure/core.clj
+++ src/lox_clojure/core.clj
@@ -1,10 +1,20 @@
 (ns lox-clojure.core
+  (:require [lox-clojure.scanner :as scanner]
+            [lox-clojure.parser :as parser]
+            [lox-clojure.ast-print :as ast-print])
   (:gen-class))
+
+(defn run
+  "To run the code, whether in REPL or file mode"
+  [source]
+  (let [tokens (scanner/scan-tokens source)
+        expression (parser/parse tokens)]
+    (ast-print/ast-print expression)))
 
 (defn run-prompt
   "To start a REPL"
   []
-  (doseq [line (line-seq (java.io.BufferedReader. *in*))] (println line)))
+  (doseq [line (line-seq (java.io.BufferedReader. *in*))] (run line)))
 
 (defn -main
   "I don't do a whole lot ... yet."
@@ -12,6 +22,5 @@
   (case (count args)
     0 (run-prompt)
     1 (println "not implemented")
-    (do (println "Usage: jlox [script]") (System/exit 64)))
-  (println "Hello, World!"))
+    (do (println "Usage: jlox [script]") (System/exit 64))))
 
